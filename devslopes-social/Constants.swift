83cbe6a8f52f90bf993b7bc019036512c1ccee5d
khavariali <khavariali@yahoo.com>
2016-12-07 15:36:58 +0800

    sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Allen on 07/12/2016.
+//  Copyright © 2016 IT Emergency Malaysia. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
