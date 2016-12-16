9d73566859e1e7fb652d4fba0f40cda31fa0b2f3
khavariali <khavariali@yahoo.com>
2016-12-16 21:22:13 +0800

    Parsing data from friebase andprinting it in the console

--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -13,19 +13,26 @@ import SwiftKeychainWrapper
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    
+    var posts = [Post]()
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         
-        DataService.ds.REF_POSTS.observe(.value) { (snapshot) in
-            print(snapshot.value!)
-        }
-        
-//        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-//            print("this is a test : \(snapshot.value!)")
-//        })
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            self.posts = []
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP:\(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+        })
     }
     
     
@@ -37,10 +44,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("***REZA*** \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell")as! PostCell
     }
     
